51136e1d89818f91812c5f8ba0b54a5ce5dd76e6
samuelreichor <samuelreichor@gmail.com>
2022-12-27 12:03:55 +0100

    Add navigation style

---
+++ src/_components/button/button.vue
@@ -0,0 +1,31 @@
+<template>
+    <div :class="name">
+        <div>
+            <a :href="btnLink" class="bg-primary text-black text-base px-8 py-2.5 gap-3 inline-flex border-2 border-black transition-all hover:bg-transparent hover:border-2 hover:border-primary hover:text-white">
+                {{btnText}}
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+        },
+        props: {
+            btnText: {
+                type: String,
+                default: () => 'Termine',
+            },
+            btnLink: {
+                type: String,
+                default: () => '/',
+            }
+        },
+        data() {
+            return {
+                name: 'c-button',
+            }
+        },
+    }
+</script>
--- src/_components/navigation/navigation.vue
+++ src/_components/navigation/navigation.vue
@@ -1,31 +1,47 @@
 <template>
     <div :class="name">
-        <div class="container">
-            <ul class="col-span-full flex justify-end gap-x-6">
-                <li>
-                    <a href="/" class="text-lg">
-                        Home
+        <div class="bg-black h-16">
+            <div class="container h-full flex items-center justify-between">
+                <div class="h-10">
+                    <a href="/">
+                        <img src="/dist/assets/images/png/Logo-light.png" height="10" width="20" alt="logo" class="h-full w-auto">
                     </a>
-                </li>
-                <li>
-                    <a href="/about" class="text-lg">
-                        About
-                    </a>
-                </li>
-            </ul>
+                </div>
+                <div class="block md:hidden h-6">
+                    <img src="/dist/assets/images/svg/hamburger.svg" class="h-full w-auto">
+                </div>
+                <ul class="hidden md:flex items-center gap-x-10 text-gray-100">
+                    <li>
+                        <a href="/" class="text-base">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/about" class="text-base">
+                            Über uns
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/information" class="text-base">
+                            Informationen
+                        </a>
+                    </li>
+                    <li>
+                        <div>
+                            <Button btnText="Termine"/>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Button from '../button/button.vue';
     export default {
         components: {
-        },
-        props: {
-            exampleProp: {
-                type: String,
-                default: () => '',
-            },
+            Button,
         },
         data() {
             return {
